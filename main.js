ba7a7af30d7e43958447eb23c17dd9f1cb3d6410
lemanrza <lemaner@code.edu.az>
2025-04-14 01:21:46 +0400

    sort and search

--- main.js
+++ main.js
@@ -2,20 +2,32 @@ let removeText = document.querySelector(".remove-text");
 let todo = document.querySelector(".todo");
 let ul = document.querySelector("ul");
 let addTodo = document.querySelector(".addToDo");
-let addBtn = document.querySelector(".ending");
+let plus = document.querySelector(".plus");
+let addToList = document.querySelector(".add-text");
+let sortIcon = document.querySelector(".sortIcons");
+let sortAlph = sortIcon.querySelector("i:first-child")
+let sortAlphReverse = sortIcon.querySelector("i:last-child")
+let listElements = ul.querySelectorAll(".list")
+let searchInput=document.querySelector(".search")
 
 removeText.addEventListener("click", () => {
     todo.value = "";
 });
 
-addBtn.addEventListener("click", () => {
-    if (addTodo.style.display === "none") {
+plus.addEventListener("click", () => {
+    if (addTodo.style.display === "none" || addTodo.style.display === "") {
         addTodo.style.display = "flex";
         todo.focus();
-        return;
     }
+    else if (ul.children.length == 0) {
+        addTodo.style.display = "flex"
+    } else {
+        addTodo.style.display = "none";
+    }
+});
 
-    if (todo.value.trim() !== "") {
+addToList.addEventListener("click", () => {
+    if (todo.value !== "") {
         let li = document.createElement("li");
         li.classList.add("list");
         li.innerHTML = `
@@ -30,23 +42,47 @@ addBtn.addEventListener("click", () => {
         todo.value = "";
         addTodo.style.display = "none";
 
-        Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Your work has been saved",
-            showConfirmButton: false,
-            timer: 1500
-        });
-
         const deleteBtn = li.querySelector(".xmark");
         deleteBtn.addEventListener("click", () => {
             li.remove();
             if (ul.children.length === 0) {
                 ul.style.display = "none";
-                addTodo.style.display = "flex"; 
+                addTodo.style.display = "flex";
             }
         });
     } else {
-        Swal.fire("Input is empty!");
+        console.log("Add ToDo please");
     }
 });
+todo.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        addToList.click();
+    }
+});
+
+let flag = true
+sortIcon.addEventListener("click", () => {
+    flag = !flag
+
+    sortAlphReverse.style.display = flag ? "inline-block" : "none"
+    sortAlph.style.display = flag ? "none" : "inline-block"
+
+    const listArr = [...ul.querySelectorAll(".list")]
+    listArr.sort((a, b) => {
+        let first = a.querySelector("div").textContent.toLowerCase()
+        let second = b.querySelector("div").textContent.toLowerCase()
+        return flag ? second.localeCompare(first) : first.localeCompare(second)
+    })
+    ul.innerHTML = "";
+listArr.forEach(li => ul.appendChild(li));
+})
+
+searchInput.addEventListener("keyup", ()=>{
+    let allElements=ul.querySelectorAll(".list")
+    allElements.forEach(element=>{
+const text= element.querySelector("div").textContent.trim().toLowerCase().includes(searchInput.value.toLowerCase().trim())
+element.style.display=text ? "flex" : "none"
+    })
+})
+
+
